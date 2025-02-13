5304164651fb960475820141c642803b36c1acd5
Artur Logan <arturlogan1ferreira@gmail.com>
2025-02-13 12:30:00 -0300

    Commit do projeto, necessita de correção

--- src/main/java/com/arturlogan/ebac_logs/EbacLogsApplication.java
+++ src/main/java/com/arturlogan/ebac_logs/EbacLogsApplication.java
@@ -2,12 +2,19 @@ package com.arturlogan.ebac_logs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.arturlogan.ebac_logs")
 public class EbacLogsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EbacLogsApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
---
+++ src/main/java/com/arturlogan/ebac_logs/controller/LogsController.java
@@ -0,0 +1,42 @@
+package com.arturlogan.ebac_logs.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/logs")
+public class LogsController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(LogsController.class);
+    @Value("${servico.url1:http://localhost:8082}")
+    private String urlSistema2;
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping
+    public String mostrarMensagem(){
+        final double mensagem = Math.random();
+
+        LOGGER.info("Gerei uma mensagem {}", mensagem);
+
+        if (urlSistema2.length() > 0){
+            final String mensagemDeles = restTemplate.exchange(urlSistema2, HttpMethod.GET, null, new ParameterizedTypeReference<String>() {
+            }).getBody();
+
+            return "Eu disse isso: " + mensagem + " e eles disseram isso: (" + mensagemDeles + ")";
+        }
+
+        return "E eu deveria parar aqui";
+    }
+
+}
